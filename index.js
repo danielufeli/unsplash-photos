ad2223c400488145669a78f0fe9babd903d878b2
Daniel Ufeli <danielufeli@yahoo.com>
2020-01-17 00:44:35 +0100

    Deploy to heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const webpack = require('webpack');
-import 'webpack-dev-server';
-import './webpack.config';
+const WebpackDevServer = require('webpack-dev-server');
+const config = require('./webpack.config');
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
