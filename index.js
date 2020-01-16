854c02672563c50b1832c28be5057eeec73425e8
Daniel Ufeli <danielufeli@yahoo.com>
2020-01-17 00:26:24 +0100

    Deploy to heroku

---
+++ index.js
@@ -0,0 +1,18 @@
+const webpack = require('webpack');
+import 'webpack-dev-server';
+import './webpack.config';
+
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true,
+  stats: {
+    colors: true
+  }
+}).listen(3000, 'localhost', function (err) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log('Listening at localhost:3000');
+});
