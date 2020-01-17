dc6d1a0b45a667100b6eeebd5088215c932f8e2d
Daniel Ufeli <danielufeli@yahoo.com>
2020-01-17 01:39:33 +0100

    deploy to heruko

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: config.get('publicPath')
+        publicPath: ''
     },
 
     // module/loaders configuration
