f15b2fa178eb4979e240b335a9588fedf0bf0754
Daniel Ufeli <danielufeli@yahoo.com>
2020-01-17 01:21:56 +0100

    Deploy to heroku

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname));
 
 // send the user to index html page inspite of the url
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'dist/index.html'));
 });
 
 app.listen(port);
